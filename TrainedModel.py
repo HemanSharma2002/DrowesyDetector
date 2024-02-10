16db66e4ec71109caa7f3c1f4ffe001ed97d761d
HemanSharma2002 <hemansharma2002@gmail.com>
2024-02-10 16:54:12 +0530

    First Commit

---
+++ TrainedModel.py
@@ -0,0 +1,36 @@
+import uuid
+import os
+import time
+
+import inline
+import matplotlib
+import torch
+from matplotlib import pyplot as plt
+import numpy as np
+import cv2
+import torch
+import cv2
+#Importing the model
+
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='yolov5/runs/train/exp5/weights/last.pt', force_reload=True)
+
+# Sample test run
+
+# img = os.path.join('data', 'images', 'awake.8b4294e5-c7f0-11ee-84ae-b40ede9bed9d.jpg')
+# results = model(img)
+# results.print()
+
+# To rune the new trained model
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
+    ret, frame = cap.read()
+
+    # Make detections
+    results = model(frame)
+
+    cv2.imshow('YOLO', np.squeeze(results.render()))
+
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
